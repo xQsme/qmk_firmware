2e9de99767bb7c187517d9c5b575a02021203c0e
solartempest <82990256+solartempest@users.noreply.github.com>
2021-11-25 01:28:45 -0500

    Space optimizations, code cleanup, added code comments.

--- keyboards/solartempest/sofle/keymaps/via/encoder.c
+++ keyboards/solartempest/sofle/keymaps/via/encoder.c
@@ -18,15 +18,15 @@
 
 
 #ifdef ENCODER_ENABLE
-	static uint8_t  encoder_state[2] = {0};	//Thanks to Drashna for this improved code with better extensibility.
-	static keypos_t encoder_ccw[2] = {{6, 3}, {6, 8}};
+	static uint8_t  encoder_state[2] = {0};
+	static keypos_t encoder_ccw[2] = {{6, 3}, {6, 8}}; //Use these keymap positions to specify the encoder functions on rotate.
 	static keypos_t encoder_cw[2] = {{6, 1}, {6, 6}};
 
 	void encoder_action_unregister(void) {
 		for (int index = 0; index < 2; ++index) {
 			if (encoder_state[index]) {
 				keyevent_t encoder_event = (keyevent_t){.key = encoder_state[index] >> 1 ? encoder_cw[index] : encoder_ccw[index], .pressed = false, .time = (timer_read() | 1)};
-				encoder_state[index]     = 0;
+				encoder_state[index] = 0;
 				action_exec(encoder_event);
 			}
 		}
@@ -34,7 +34,7 @@
 
 	void encoder_action_register(uint8_t index, bool clockwise) {
 		keyevent_t encoder_event = (keyevent_t){.key = clockwise ? encoder_cw[index] : encoder_ccw[index], .pressed = true, .time = (timer_read() | 1)};
-		encoder_state[index]     = (clockwise ^ 1) | (clockwise << 1);
+		encoder_state[index] = (clockwise ^ 1) | (clockwise << 1);
 		action_exec(encoder_event);
 	}
 
--- keyboards/solartempest/sofle/rules.mk
+++ keyboards/solartempest/sofle/rules.mk
@@ -5,26 +5,26 @@ BOOTLOADER = atmel-dfu
 # Comment out to disable the options.
 
 SPLIT_KEYBOARD = yes			# Split keyboard
-BOOTMAGIC_ENABLE = no			# Virtual DIP switch configuration
-CONSOLE_ENABLE = no				# Console for debug
-EXTRAKEY_ENABLE = yes			# Audio control and System control (+450)
-RGBLIGHT_ENABLE = yes			# RGB lighting (+2350)
-ENCODER_ENABLE = yes			# Encoders
-MOUSEKEY_ENABLE = no			# Mouse keys (+1482). Not required for trackball usage.
-NKRO_ENABLE = no				# Has 6KRO+ already. NKRO does not work for the sofle.
-COMBO_ENABLE = no				# Tap combo chording (+952)
-LTO_ENABLE = yes				# Save space
-
-OLED_ENABLE = yes				# Must be disabled if OLEDs are not installed (+3608-4774)
-OLED_DRIVER = SSD1306			# Specify OLED driver
-WPM_ENABLE = no					# WPM counter for keyboard pets (+1298)
-								# Via_enable in keymap (+2300). VIA is required as non-via layouts are removed to save space.
-						
+CONSOLE_ENABLE = no				# Console for debug (+4686)
+BOOTMAGIC_ENABLE = no			# Virtual DIP switch configuration (+644)
+MAGIC_ENABLE = no				# Save space by not PROCESS_MAGIC, which is bootmagic with out the boot (+634)
 GRAVE_ESC_ENABLE = no			# Save space by not using KC_GESC (+127)
-MAGIC_ENABLE = no				# Save space by not PROCESS_MAGIC, which is bootmagic with out the boot (+612)
 SPACE_CADET_ENABLE = no 		# Save space by not using space cadet left/right shift (+350)
+LTO_ENABLE = yes				# Save space by link time optimization
 
-POINTING_DEVICE_ENABLE = yes	# Enable Pimoroni Trackball (+2304)
+EXTRAKEY_ENABLE = yes			# Audio control and System control (+450)
+RGBLIGHT_ENABLE = yes			# RGB lighting (+3826)
+ENCODER_ENABLE = yes			# Rotary encoders (+1112)
+MOUSEKEY_ENABLE = no			# Mouse keys (+1482). Not required for trackball usage.
+NKRO_ENABLE = no				# 6KRO by default. NKRO works if enabled (+328)
+COMBO_ENABLE = no				# Tap combo chording (+952)
+#VIA							# VIA_ENABLE with the keymap (+2300). Note: VIA is required as non-via layouts were removed to save space.
+
+OLED_ENABLE = yes				# Must be disabled if OLEDs are not installed (+3608-4774 with pet)
+OLED_DRIVER = SSD1306			# Specify OLED driver
+WPM_ENABLE = no					# WPM counter for typing-speed-reactive keyboard pets (+1298)
+
+POINTING_DEVICE_ENABLE = yes	# Enable Pimoroni Trackball on master (+2304)
 QUANTUM_LIB_SRC += i2c_master.c
 SRC += drivers/sensors/pimoroni_trackball.c
 

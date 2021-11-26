98968abd8340971608ad49e5cda70ec8a63d4ce3
solartempest <82990256+solartempest@users.noreply.github.com>
2021-11-25 20:36:23 -0500

    Improved stretch timer LEDs. Add NKRO.

--- keyboards/solartempest/9e/config.h
+++ keyboards/solartempest/9e/config.h
@@ -27,34 +27,28 @@
 #define PRODUCT         A9e
 #define DESCRIPTION     Keyboard
 
-/* key matrix size */
+// key matrix settings
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
-
-/* key matrix pins */
 #define MATRIX_ROW_PINS { F0, F1, F2, F3 }
-#define MATRIX_COL_PINS { F5, F6, F7, A0, A1, A2, A3, A4, A5, B7, B6, B5 } //F5, F7, B7, B5 are unused on the MCU. They are needed for the encoder key matrix.
-#define UNUSED_PINS
+#define MATRIX_COL_PINS { F5, F6, F7, A0, A1, A2, A3, A4, A5, B7, B6, B5 } //F5, F7, B7, B5 are unused on the MCU. Necessary for encoder key matrix.
+#define DIODE_DIRECTION COL2ROW
+#define DEBOUNCE 6 //Default is 5
+
+// NKRO Settings
+#ifdef NKRO_ENABLE
+	#define FORCE_NKRO //Force NKRO if enabled.
+#endif
+
+#define LOCKING_SUPPORT_ENABLE //Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
+#define LOCKING_RESYNC_ENABLE //Locking resynchronize hack
 
 /* encoder pins */
 #define ENCODERS_PAD_A { C0, C3, C5, C7, D1, D3, D5, D6, A7 } //Encoder pads ordered so on VIA mapping it is cw/ccw and up/down rotation orientation.
 #define ENCODERS_PAD_B { C1, C2, C4, C6, D0, D2, D4, D7, A6 }
 #define ENCODER_RESOLUTIONS { 4, 4, 4, 4, 4, 4, 4, 2, 2 } //Better control of individual encoders and encoder types.
-//#define ENCODER_RESOLUTION 4
 
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-
+//RGB Settings
 #define RGB_DI_PIN E6
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_EFFECT_SNAKE_LENGTH 1 //Default 4
